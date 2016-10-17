9f7ec018777f5c9d78e8ef43383da8a2e405f60e
greggles <greggles@36762.no-reply.drupal.org>
2016-10-17 08:17:58 -0600

    Issue #2818549 by greggles: Update for Drupal code style

--- email_confirm.api.php
+++ email_confirm.api.php
@@ -1,6 +1,11 @@
 <?php
 
 /**
+ * @file
+ * Document api for Email Change Confirmation module.
+ */
+
+/**
  * Act on email change confirmation events.
  *
  * This hook allows a module to respond to either the request to change an
@@ -10,9 +15,9 @@
  *   Either "email change" or "email confirmation".
  * @param int $uid
  *   The user's uid.
- * @param $old_mail
+ * @param string $old_mail
  *   The user's old email.
- * @param $new_mail
+ * @param string $new_mail
  *   The user's new email.
  */
 function hook_email_confirm($op, $uid, $old_mail, $new_mail) {
